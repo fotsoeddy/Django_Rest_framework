264a8a990c3c09dbb4c24004246911c6763294d2
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-03-13 13:22:52 +0100

    serializer created for the APi

--- Auth_API/blog/views.py
+++ Auth_API/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        if serializer.instance.author == self.request.user:
+            serializer.save()
+        else:
+            raise serializers.ValidationError("You are not the author of this post.")
+
+    def perform_destroy(self, instance):
+        if instance.author == self.request.user:
+            instance.delete()
+        else:
+            raise serializers.ValidationError("You are not the author of this post.")
