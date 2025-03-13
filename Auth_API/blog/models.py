0addc905a3629ba79b74c92a75006df87ce3ad8d
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-03-13 13:29:41 +0100

    updated the fields in the model

--- Auth_API/blog/models.py
+++ Auth_API/blog/models.py
@@ -5,6 +5,9 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)  # For blog images
+    tags = models.CharField(max_length=255, blank=True)  # For tags (e.g., "tech, programming")
+    category = models.CharField(max_length=100, blank=True)  # For category (e.g., "Technology")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
